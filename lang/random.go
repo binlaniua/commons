27083308c878eda3442575f55cec599eb6818bae
Binlaniua <Binlaniua@gmail.com>
2016-11-12 22:51:11 +0800

    make module

---
+++ lang/random.go
@@ -0,0 +1,16 @@
+package lang
+
+// Random contain number and letter
+func Random(count int) string {
+	return ""
+}
+
+// RandomLetter contain letter
+func RandomLetter(count int) string {
+	return ""
+}
+
+// RandomNumber contain number
+func RandomNumber(count int) string {
+	return ""
+}
